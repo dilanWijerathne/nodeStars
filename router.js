41b91a2203ad1a0f405cb88ed5b3f2a3d29c6463
Dilan Wijerathne <dilan.w@findmyfare.com>
2016-03-12 12:44:49 +0530

    route from server hyperlooop removed

--- router.js
+++ router.js
@@ -2,9 +2,7 @@
 
 function route(pathname){
 	console.log("About to route a request for "+pathname);
-	for(var i =1; i>0;i++){
-		console.log(i+" : Hyper loop "+pathname);
-	}
+	
 }
 
 exports.route = route;
