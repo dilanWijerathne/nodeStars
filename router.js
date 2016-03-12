51cd738049560128320b86542ec4abe1def3fe9d
Dilan Wijerathne <dilan.w@findmyfare.com>
2016-03-12 12:36:06 +0530

    route from server hyperlooop

--- router.js
+++ router.js
@@ -2,6 +2,9 @@
 
 function route(pathname){
 	console.log("About to route a request for "+pathname);
+	for(var i =0; i<5000;i++){
+		console.log("Hyper loop "+pathname);
+	}
 }
 
 exports.route = route;
